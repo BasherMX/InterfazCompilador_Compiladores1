2ca1ca3e6932224f8edaa62336d9a626235a7e96
Marcela <mayorohe.0606@gmail.com>
2023-02-23 10:43:39 -0600

    first commit

---
+++ Interfaz_Compilador/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿using System.Drawing;
+
+namespace Interfaz_Compilador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personalizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modoClaroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modoOscuroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(183)))), ((int)(((byte)(215)))));
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.herramientasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 6, 0, 6);
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(1281, 39);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.abrirToolStripMenuItem,
+            this.toolStripSeparator,
+            this.guardarToolStripMenuItem,
+            this.guardarComoToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.toolStripSeparator2,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(81, 27);
+            this.archivoToolStripMenuItem.Text = "&Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("nuevoToolStripMenuItem.Image")));
+            this.nuevoToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(214, 28);
+            this.nuevoToolStripMenuItem.Text = "&Nuevo";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("abrirToolStripMenuItem.Image")));
+            this.abrirToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(214, 28);
+            this.abrirToolStripMenuItem.Text = "&Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(211, 6);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("guardarToolStripMenuItem.Image")));
+            this.guardarToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(214, 28);
+            this.guardarToolStripMenuItem.Text = "&Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(214, 28);
+            this.guardarComoToolStripMenuItem.Text = "Guardar Como";
+            this.guardarComoToolStripMenuItem.Click += new System.EventHandler(this.guardarComoToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(211, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(211, 6);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(214, 28);
+            this.salirToolStripMenuItem.Text = "&Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.personalizarToolStripMenuItem});
+            this.herramientasToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(126, 27);
+            this.herramientasToolStripMenuItem.Text = "&Herramientas";
+            // 
+            // personalizarToolStripMenuItem
+            // 
+            this.personalizarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modoClaroToolStripMenuItem,
+            this.modoOscuroToolStripMenuItem});
+            this.personalizarToolStripMenuItem.Name = "personalizarToolStripMenuItem";
+            this.personalizarToolStripMenuItem.Size = new System.Drawing.Size(185, 28);
+            this.personalizarToolStripMenuItem.Text = "&Personalizar";
+            // 
+            // modoClaroToolStripMenuItem
+            // 
+            this.modoClaroToolStripMenuItem.Name = "modoClaroToolStripMenuItem";
+            this.modoClaroToolStripMenuItem.Size = new System.Drawing.Size(198, 28);
+            this.modoClaroToolStripMenuItem.Text = "Modo Claro";
+            this.modoClaroToolStripMenuItem.Click += new System.EventHandler(this.modoClaroToolStripMenuItem_Click);
+            // 
+            // modoOscuroToolStripMenuItem
+            // 
+            this.modoOscuroToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
+            this.modoOscuroToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.modoOscuroToolStripMenuItem.Name = "modoOscuroToolStripMenuItem";
+            this.modoOscuroToolStripMenuItem.Size = new System.Drawing.Size(198, 28);
+            this.modoOscuroToolStripMenuItem.Text = "Modo Oscuro";
+            this.modoOscuroToolStripMenuItem.Click += new System.EventHandler(this.modoOscuroToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(33, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1000, 500);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(237)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(1281, 896);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "IngeScript";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personalizarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modoClaroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modoOscuroToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem guardarComoToolStripMenuItem;
+    }
+}
+
---
+++ Interfaz_Compilador/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Interfaz_Compilador
+{
+    public partial class Form1 : Form
+    {
+
+        private string currentFilePath = "";
+        Color commentColor = Color.FromArgb(170,170,170);
+        Color symbolColor = Color.FromArgb(4,51,250);
+        Color stringColor = Color.FromArgb(68,140,39);
+        Color numberColor = Color.FromArgb(156,95,43);
+        Color keywordColor = Color.FromArgb(122,62,157);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        // Evento Click del botón "Nuevo"
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentFilePath = "";
+            richTextBox1.Text = "";
+        }
+
+        // Evento Click del botón "Abrir"
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = openFileDialog1.FileName;
+                try
+                {
+                    richTextBox1.Text = File.ReadAllText(currentFilePath);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error al abrir el archivo");
+                }
+            }
+
+        }
+
+        // Evento Click del botón "Guardar"
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                guardarComoToolStripMenuItem_Click(sender, e);
+            }
+            else
+            {
+                try
+                {
+                    File.WriteAllText(currentFilePath, richTextBox1.Text);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error al guardar el archivo");
+                }
+            }
+        }
+
+        // Evento Click del botón "Guardar Como"
+        private void guardarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = saveFileDialog1.FileName;
+
+                try
+                {
+                    File.WriteAllText(currentFilePath, richTextBox1.Text);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error al guardar el archivo");
+                }
+            }
+
+        }
+
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void modoOscuroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            commentColor = Color.FromArgb(122, 135, 182);
+            symbolColor = Color.FromArgb(255, 215, 5);
+            stringColor = Color.FromArgb(194, 237, 127);
+            numberColor = Color.FromArgb(120, 204, 240);
+            keywordColor = Color.FromArgb(199, 146, 232);
+            this.BackColor = Color.FromArgb(49,54,74);
+            this.ForeColor = Color.White;
+            richTextBox1.BackColor = Color.FromArgb(41,45,62);
+            richTextBox1.ForeColor = this.ForeColor;
+            menuStrip1.BackColor = Color.FromArgb(37, 41, 58);
+            menuStrip1.ForeColor = this.ForeColor;
+        }
+
+
+        private void modoClaroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            commentColor = Color.FromArgb(136, 136, 136);
+            symbolColor = Color.FromArgb(4, 51, 250);
+            stringColor = Color.FromArgb(68, 140, 39);
+            numberColor = Color.FromArgb(156, 95, 43);
+            keywordColor = Color.FromArgb(115,87,154);
+            this.BackColor = Color.FromArgb(237,237,245);
+            this.ForeColor = Color.Black;
+            richTextBox1.BackColor = Color.FromArgb(245,245,245);
+            richTextBox1.ForeColor = this.ForeColor;
+            menuStrip1.BackColor = Color.FromArgb(196,183,215);
+            menuStrip1.ForeColor = this.ForeColor;
+        }
+
+      
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            string[] keywords = { "int", "float", "double", "string", "using", "namespace", "class", "public", "private", "protected", "void", "return" };
+            int selectionStart = richTextBox1.SelectionStart;
+            int selectionLength = richTextBox1.SelectionLength;
+
+            // Colorea las palabras clave
+
+            foreach (string palabraClave in keywords)
+            {
+                int index = 0;
+                while (index < richTextBox1.TextLength)
+                {
+                    int indexOfKeyword = richTextBox1.Find(palabraClave, index, richTextBox1.TextLength, RichTextBoxFinds.WholeWord);
+                    if (indexOfKeyword != -1)
+                    {
+                        richTextBox1.SelectionStart = indexOfKeyword;
+                        richTextBox1.SelectionLength = palabraClave.Length;
+                        richTextBox1.SelectionFont = new Font(richTextBox1.Font, FontStyle.Bold);
+                        richTextBox1.SelectionColor = keywordColor;
+                        index = indexOfKeyword + palabraClave.Length;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < richTextBox1.Text.Length; i++)
+            {
+               
+
+                // Colorea los símbolos
+                if (!char.IsLetterOrDigit(richTextBox1.Text[i]))
+                {
+                    richTextBox1.Select(i, 1);
+                    richTextBox1.SelectionColor = symbolColor;
+                }
+                // Colorea las cadenas de texto encerradas en comillas dobles
+                if (richTextBox1.Text[i] == '\"')
+                {
+                    int j = i + 1;
+                    while (j < richTextBox1.Text.Length && richTextBox1.Text[j] != '\"')
+                    {
+                        j++;
+                    }
+                    if (j < richTextBox1.Text.Length && richTextBox1.Text[j] == '\"')
+                    {
+                        richTextBox1.Select(i, j - i + 1);
+                        richTextBox1.SelectionColor = stringColor;
+                        i = j;
+                    }
+                }
+                // Colorea las cadenas de texto encerradas en comillas simples
+                if (richTextBox1.Text[i] == '\'')
+                {
+                    int j = i + 1;
+                    while (j < richTextBox1.Text.Length && richTextBox1.Text[j] != '\'')
+                    {
+                        j++;
+                    }
+                    if (j < richTextBox1.Text.Length && richTextBox1.Text[j] == '\'')
+                    {
+                        richTextBox1.Select(i, j - i + 1);
+                        richTextBox1.SelectionColor = stringColor;
+                        i = j;
+                    }
+                }
+                // Colorea los números
+                if (char.IsDigit(richTextBox1.Text[i]))
+                {
+                    int j = i + 1;
+                    while (j < richTextBox1.Text.Length && char.IsDigit(richTextBox1.Text[j]))
+                    {
+                        j++;
+                    }
+                    if (j < richTextBox1.Text.Length && richTextBox1.Text[j] == '.')
+                    {
+                        j++;
+                        while (j < richTextBox1.Text.Length && char.IsDigit(richTextBox1.Text[j]))
+                        {
+                            j++;
+                        }
+                    }
+                    if (j < richTextBox1.Text.Length && (richTextBox1.Text[j] == 'f' || richTextBox1.Text[j] == 'F'))
+                    {
+                        j++;
+                    }
+                    richTextBox1.Select(i, j - i);
+                    richTextBox1.SelectionColor = numberColor;
+                    i = j - 1;
+                }               
+
+                // Colorea los comentarios de una línea
+                if (richTextBox1.Text[i] == '/' && i + 1 < richTextBox1.Text.Length && richTextBox1.Text[i + 1] == '/')
+                {
+                    int j = i + 2;
+                    while (j < richTextBox1.Text.Length && richTextBox1.Text[j] != '\n')
+                    {
+                        j++;
+                    }
+                    richTextBox1.Select(i, j - i);
+                    richTextBox1.SelectionFont = new Font(richTextBox1.Font, FontStyle.Italic);
+                    richTextBox1.SelectionColor = commentColor;
+                    i = j - 1;
+                }
+                // Colorea los comentarios de varias líneas
+                if (richTextBox1.Text[i] == '/' && i + 1 < richTextBox1.Text.Length && richTextBox1.Text[i + 1] == '*')
+                {
+                    int j = i + 2;
+                    while (j < richTextBox1.Text.Length && (richTextBox1.Text[j] != '*' || j + 1 >= richTextBox1.Text.Length || richTextBox1.Text[j + 1] != '/'))
+                    {
+                        j++;
+                    }
+                    if (j + 1 < richTextBox1.Text.Length && richTextBox1.Text[j] == '*' && richTextBox1.Text[j + 1] == '/')
+                    {
+                        j += 2;
+                    }
+
+                    
+                    richTextBox1.Select(i, j - i);
+                    richTextBox1.SelectionFont = new Font(richTextBox1.Font, FontStyle.Italic);
+                    richTextBox1.SelectionColor = commentColor;
+                    i = j - 1;
+
+                }
+                // Restaura la selección original
+               
+                richTextBox1.SelectionStart = selectionStart;
+                richTextBox1.SelectionLength = selectionLength;
+                richTextBox1.SelectionColor = Color.Black;
+
+            }
+        }
+    }
+}
+    
+
+
+
+
+
+
